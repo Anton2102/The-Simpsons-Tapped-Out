a5b64ab0c1d5440807d10100e11b9193889b7413
Anton2102 <leshonkov1@gmail.com>
2020-07-23 21:45:27 +0300

    Написал функцию получения страничек.

--- index.js
+++ index.js
@@ -6,8 +6,63 @@ const fs = require('fs');
 // Запуск сайта по адресу ( http://localhost:8888 ).
 http.createServer( (request, response) => {
   if (request.url != 'favicon.ico') {
-    response.writeHeader(200, {'Content-Type': 'text.html'});
-    response.write('test start!');
-    response.end();
+
+    if (request.url.endsWith('.css')) {
+      let cssFile = request.url.slice(1);
+      fs.readFile(cssFile, (err, data) => {
+        if (err) throw err;
+
+        response.setHeader('Content-Type', 'text/css');
+        response.statusCode = 200;
+        response.write(data);
+        response.end();
+      });
+    } else if (request.url.endsWith('.js')) {
+      let jsFile = request.url.slice(1);
+      fs.readFile(jsFile, (err, data) => {
+        if (err) throw err;
+
+        response.setHeader('Content-Type', 'text/javascript');
+        response.statusCode = 200;
+        response.write(data);
+        response.end();
+      });
+    } else if (request.url.endsWith('.jpg')) {
+      let imgFile = request.url.slice(1);
+      fs.readFile(imgFile, (err, data) => {
+        if (err) throw err;
+
+        response.setHeader('Content-Type', 'image/jpg');
+        response.statusCode = 200;
+        response.write(data);
+        response.end();
+      });
+
+    } else {
+      getPage(request.url, response);
+    }
+
   }
 }).listen(8888);
+
+function getPage(name, response, statusCode = 200) {
+  if (name == '/'){
+    name = 'index';
+  }
+
+  fs.readFile('pages/' + name + '.html', 'utf8', (err, data) => {
+    // if (err) throw err;
+    if (!err) {
+      response.setHeader('Content-Type', 'text/html');
+      response.statusCode = statusCode;
+      response.write(data);
+      response.end();
+    } else {
+      if (name != '404'){
+        getPage('404', response, 404);
+      } else {
+        throw err;
+      }
+    }
+  });
+}
