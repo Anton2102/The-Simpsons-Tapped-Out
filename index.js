76b9a9faa8d4544b7a1493313ad47da8b62bdb55
Anton2102 <leshonkov1@gmail.com>
2020-07-27 09:31:29 +0300

    Добвал чтения фалов с другими расширениями

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 // Запуск сайта по адресу ( http://localhost:8888 ).
 http.createServer( (request, response) => {
-  if (request.url != 'favicon.ico') {
+  if (request.url != '/favicon.ico'){
 
     if (request.url.endsWith('.css')) {
       let cssFile = request.url.slice(1);
@@ -41,9 +41,31 @@ http.createServer( (request, response) => {
     } else {
       getPage(request.url, response);
     }
-
   }
 }).listen(8888);
+
+// function getOtherFile(file, format){
+//   // let sliceNameFile = request.url.slice(1);
+//   let imgFile = request.url.slice(1);
+//   fs.readFile(imgFile, (err, data) => {
+//     if (err) throw err;
+//
+//     response.setHeader('Content-Type', 'image/jpg');
+//     response.statusCode = 200;
+//     response.write(data);
+//     response.end();
+//   });
+  // fs.readFile(sliceNameFile, (err, data) => {
+  //
+  //   // if (err) throw err;
+  //
+  //   response.setHeader('Content-Type', format);
+  //   response.statusCode = 200;
+  //   response.write(data);
+  //   response.end();
+  // });
+
+// }
 
 function getPage(name, response, statusCode = 200) {
   if (name == '/'){
@@ -51,18 +73,21 @@ function getPage(name, response, statusCode = 200) {
   }
 
   fs.readFile('pages/' + name + '.html', 'utf8', (err, data) => {
-    // if (err) throw err;
+
     if (!err) {
       response.setHeader('Content-Type', 'text/html');
       response.statusCode = statusCode;
       response.write(data);
       response.end();
     } else {
+
       if (name != '404'){
         getPage('404', response, 404);
       } else {
         throw err;
       }
+
     }
   });
+
 }
