7af0c2bff5f6958710ec74d56693d7c621a2c0d2
Anton2102 <leshonkov1@gmail.com>
2020-07-27 17:16:23 +0300

    разрезал страницу на части(сделал шаблон).

--- index.js
+++ index.js
@@ -40,10 +40,38 @@ function getPage(name, response, statusCode = 200) {
   fs.readFile('pages/' + name + '.html', 'utf8', (err, data) => {
 
     if (!err) {
-      response.setHeader('Content-Type', 'text/html');
-      response.statusCode = statusCode;
-      response.write(data);
-      response.end();
+
+      fs.readFile('elems/linksHeader.html', 'utf8', (err, linksHeader) => {
+        if (err) throw err;
+
+        data = data.replace(/\{\{linksHeader\}\}/g, linksHeader);
+
+        fs.readFile('elems/menu.html', 'utf8', (err, menu) => {
+          if (err) throw err;
+
+          data = data.replace(/\{\{menu\}\}/g, menu);
+
+          fs.readFile('elems/main.html', 'utf8', (err, main) => {
+            if (err) throw err;
+
+            data = data.replace(/\{\{main\}\}/g, main);
+
+            fs.readFile('elems/footer.html', 'utf8', (err, footer) => {
+              if (err) throw err;
+
+              data = data.replace(/\{\{footer\}\}/g, footer);
+
+              response.setHeader('Content-Type', 'text/html');
+              response.statusCode = statusCode;
+              response.write(data);
+              response.end();
+            });
+
+          });
+
+        });
+
+      });
     } else {
 
       if (name != '404'){
